2c6bff2a77af0aaad4110131b5bb458a6ca32aed
primakurniawan <primakurniawan666@gmail.com>
2020-10-12 22:07:39 +0700

    oct-12-2020

---
+++ modules/App.js
@@ -0,0 +1,4 @@
+import Home from "./Home";
+// const Home = require("./Home");
+// document.querySelector("body").innerHTML = Home;
+console.log(Home);
---
+++ modules/Home.js
@@ -0,0 +1,3 @@
+export default function () {
+  `<header>This is the Header</header>`;
+}
